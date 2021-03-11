4f0433b43558559d7e5f2d7aa0317eaf9ea2f5e0
Paulo silveira <paulo.silveira@usemobile.xyz>
2021-03-11 00:29:05 -0300

    chore: create home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+
+import { Container } from './StyledComponents';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default Home;
